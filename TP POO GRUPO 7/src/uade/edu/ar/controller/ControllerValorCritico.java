a3e86ef9f5d9c80ef40c38de3013fd3efabe1d08
Nirigoyen <116807507+Nirigoyen@users.noreply.github.com>
2022-11-17 10:05:16 -0300

    NO SE QUE ES ESTO PERO ME PIDE QUE LO PUSHEE

---
+++ TP POO GRUPO 7/src/uade/edu/ar/controller/ControllerValorCritico.java
@@ -0,0 +1,95 @@
+package uade.edu.ar.controller;
+
+import uade.edu.ar.dao.ValorCriticoDAO;
+import uade.edu.ar.dto.ValorCriticoDTO;
+import uade.edu.ar.model.TipoResultado;
+import uade.edu.ar.model.clases.ValorCritico;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ControllerValorCritico {
+    private ControllerValorCritico(){}
+    private ArrayList<ValorCriticoDTO> ValorCriticoDTO = new ArrayList<ValorCriticoDTO>();
+    private static List<ValorCritico> ListaValoresCriticos;
+
+    private ArrayList<ValorCriticoDTO> getValoresCriticos() {
+        return ValorCriticoDTO;
+    }
+    private static ControllerValorCritico CONTROLLERVALORC = null;
+
+    private static uade.edu.ar.dao.ValorCriticoDAO ValorCriticoDAO;
+
+   public void ModificarValorCritico(int ID, ValorCriticoDTO VCDTO, TipoResultado TipoComparacion,String Valor){
+        int i=0;
+        while (getValoresCriticos().get(i).getIDValorC() == ID) {
+            i++;}
+       getValoresCriticos().get(i).setValor(VCDTO.getValor());
+       getValoresCriticos().get(i).setTipoComparacion(VCDTO.getTipoComparacion());
+    }
+
+
+    public static synchronized ControllerValorCritico getInstances() throws Exception {
+        if(CONTROLLERVALORC == null) {
+            CONTROLLERVALORC = new ControllerValorCritico();
+            ValorCriticoDAO = new ValorCriticoDAO(ValorCritico.class,getPathOutModel(ValorCritico.class.getSimpleName()));//se crea el dao para que ya este en memoria fisica y no crearlo todo el tiempo
+            ListaValoresCriticos = ValorCriticoDAO.getAll(ValorCritico.class);//recupero la info de ese archivo creado.
+        }
+        return CONTROLLERVALORC;
+    }
+
+    public void addValorC(ValorCriticoDTO dto) throws Exception {
+        if(getByIdModel(dto.getIDValorC()) == null){
+            ValorCriticoDAO.save(toModel(dto));
+        }
+    }
+
+    public ValorCriticoDTO getByIdModel(int id) throws Exception {
+        for (ValorCritico model: ListaValoresCriticos) {
+            if (model.getIDValorC() == id){
+                return toDto(model);
+            }
+        }
+        return  null;
+    }
+
+
+    public void deleteByIdModel(int id){
+        int index = getIndex(id);
+        if(index != -1){
+            ListaValoresCriticos.remove(index);
+        }
+    }
+
+    private int getIndex(int id){
+        for (int i=0;i<ListaValoresCriticos.size();i++){
+            if(ListaValoresCriticos.get(i).getIDValorC() == id){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public ArrayList<ValorCriticoDTO> getAll() throws Exception {
+        ArrayList<ValorCriticoDTO> dtoList = new ArrayList<ValorCriticoDTO>();
+        for (ValorCritico model : ListaValoresCriticos) {
+            dtoList.add(toDto(model));
+        }
+        return dtoList;
+    }
+
+
+    private static String getPathOutModel(String name){
+        String dir = "C:/IOO/";
+        return  new File(dir+name+".json").getPath();
+    }
+
+
+    public static ValorCritico toModel(ValorCriticoDTO dto) {
+        return new ValorCritico(dto.getIDValorC(), dto.getTipoComparacion(), dto.getValor(), dto.getPracticaAsociada());
+    }
+    public static ValorCriticoDTO toDto(ValorCritico model){
+        return new ValorCriticoDTO(model.getIDValorC(),model.getValor(),model.getTipoComparacion(),model.getPracticaAsociada());
+    }
+}
