307e3bc1adef2b63477f821bfe3d7dc681a3d811
Nirigoyen <116807507+Nirigoyen@users.noreply.github.com>
2022-11-24 09:01:07 -0300

    cosa

--- TP POO GRUPO 7/src/frm_AltaSucursal.java
+++ TP POO GRUPO 7/src/frm_AltaSucursal.java
@@ -10,16 +10,26 @@ public class frm_AltaSucursal extends JFrame{
     private JTextField textField3;
     private JButton guardarButton;
     private JButton cancelarButton;
+
+    private frm_AltaSucursal self;
+
     private SucursalDTO S = new SucursalDTO( 0,"","",false);
 
     public frm_AltaSucursal() {
+        this.self = this;
+
         guardarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int ID = Integer.parseInt(textField1.getText());
                 String Direc = textField2.getText();
                 String Reesp = textField3.getText();
+
+                self.S.setNumero(ID);
+                self.S.setDireccion(Direc);
+                self.S.setResponsableTecnico(Reesp);
             }
+
 
         });
     }
