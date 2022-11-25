edf2a7caf4cb1bda6c1813ba0719c5db641a8b1e
Nirigoyen <116807507+Nirigoyen@users.noreply.github.com>
2022-11-25 20:16:20 -0300

    Empezado de cambios

---
+++ TP POO GRUPO 7/src/uade/edu/ar/dao/PracticaDAO.java
@@ -0,0 +1,12 @@
+package uade.edu.ar.dao;
+
+import uade.edu.ar.model.clases.Paciente;
+import uade.edu.ar.model.clases.Sucursal;
+import uade.edu.ar.util.GenericDAO;
+
+public class PracticaDAO extends GenericDAO {
+    public PracticaDAO(Class<Paciente> clase, String file) throws Exception {//es una clase abstracta. y se pueden usar todos los metodos de genericDAO
+        super(Sucursal.class,"C:\\Users\\Admin\\Desktop\\PacienteDAO.txt" ); //Cambiar ruta
+        //super(uade.edu.ar.model.clases.Paciente.class, "C:\\Users\\timoteo\\Desktop\\PacienteDAO.txt");
+    }
+}
---
+++ TP POO GRUPO 7/src/uade/edu/ar/dao/ResultadoDAO.java
@@ -0,0 +1,12 @@
+package uade.edu.ar.dao;
+
+import uade.edu.ar.model.clases.Paciente;
+import uade.edu.ar.model.clases.Sucursal;
+import uade.edu.ar.util.GenericDAO;
+
+public class ResultadoDAO extends GenericDAO {
+    public ResultadoDAO(Class<Paciente> clase, String file) throws Exception {//es una clase abstracta. y se pueden usar todos los metodos de genericDAO
+        super(Sucursal.class,"C:\\Users\\Admin\\Desktop\\PacienteDAO.txt" ); //Cambiar ruta
+        //super(uade.edu.ar.model.clases.Paciente.class, "C:\\Users\\timoteo\\Desktop\\PacienteDAO.txt");
+    }
+}
